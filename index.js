61e9df7cc489a5ce25e901ae2e6093b458bfc432
Simone Anthony <simone.anthony1@yahoo.com>
2021-12-15 23:50:31 -0500

    questions prompts created

---
+++ index.js
@@ -0,0 +1,94 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generatedMarkup = require('./lib/page-template');
+
+const questions = [
+    {
+      type: "input",
+      message: "What is the team manager's name?",
+      name: "managerName",
+    },
+    {
+      type: "input",
+      message: "What is the team manager's id?",
+      name: "managerId",
+    },
+    {
+        type: "input",
+        message: "What is the team manager's email?",
+        name: "managerEmail",
+      },
+    {
+      type: "input",
+      message: "What is the team manager's office number?",
+      name: "officeNumber",
+    },
+    {
+      type: "list",
+      message:
+        "Which type of team member would you like to add?",
+      pageSize: 3,
+      choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+      name: "additionalTeamMember",
+    },
+
+    // intern
+    {
+      type: "input",
+      message: "What is your intern's name?",
+      name: "internName",
+    },
+    {
+      type: "input",
+      message: "What is your intern's id?",
+      name: "internId",
+    },
+    {
+      type: "input",
+      message: "What is your intern's email?",
+      name: "internEmail",
+    },
+    {
+      type: "input",
+      message: "What is your intern's school?",
+      name: "internSchool",
+    },
+
+    // engineer
+    {
+      type: "input",
+      message: "What is your engineer's name?",
+      name: "engineerName",
+    },
+    {
+      type: "input",
+      message: "What is your engineer's id?",
+      name: "engineerId",
+    },
+    {
+      type: "input",
+      message: "What is your engineer's email?",
+      name: "engineerEmail",
+    },
+    {
+      type: "input",
+      message: "What is your engineer's GitHub username?",
+      name: "engineerGithub",
+    },
+    
+];
+
+function init() {
+    inquirer.prompt(questions).then((response) => {
+
+
+        // if (response.additionalTeamMember) return inquirer.prompt( intern or engineer question array)
+
+        // fs.writeFile('dist/index.html', generatedMarkup(), (err) => {
+        //     if (err) throw console.error('Something went wrong while generating the README');
+        //     console.log('The file has been created!');
+        // });
+    });
+}
+
+init();
