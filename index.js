a6203f07d95478afd17c385f8d22be279b7abe93
Simone Anthony <simone.anthony1@yahoo.com>
2021-12-16 21:43:57 -0500

    html template is now working

--- index.js
+++ index.js
@@ -1,98 +1,140 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const generatedMarkup = require('./lib/page-template');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generatedMarkup = require("./lib/page-template");
+// const Employee = require("./dist/Employee");
+const Manager = require("./dist/Manager");
+const Intern = require("./dist/Intern");
+const Engineer = require("./dist/Engineer");
 
-const questions = [
-    {
-      type: "input",
-      message: "What is the team manager's name?",
-      name: "managerName",
-    },
-    {
-      type: "input",
-      message: "What is the team manager's id?",
-      name: "managerId",
-    },
-    {
-        type: "input",
-        message: "What is the team manager's email?",
-        name: "managerEmail",
-      },
-    {
-      type: "input",
-      message: "What is the team manager's office number?",
-      name: "officeNumber",
-    },
-    {
-      type: "list",
-      message:
-        "Which type of team member would you like to add?",
-      pageSize: 3,
-      choices: ["Engineer", "Intern", "I don't want to add any more team members"],
-      name: "additionalTeamMember",
-    },
-
-    // intern
-    {
-      type: "input",
-      message: "What is your intern's name?",
-      name: "internName",
-    },
-    {
-      type: "input",
-      message: "What is your intern's id?",
-      name: "internId",
-    },
-    {
-      type: "input",
-      message: "What is your intern's email?",
-      name: "internEmail",
-    },
-    {
-      type: "input",
-      message: "What is your intern's school?",
-      name: "internSchool",
-    },
-
-    // engineer
-    {
-      type: "input",
-      message: "What is your engineer's name?",
-      name: "engineerName",
-    },
-    {
-      type: "input",
-      message: "What is your engineer's id?",
-      name: "engineerId",
-    },
-    {
-      type: "input",
-      message: "What is your engineer's email?",
-      name: "engineerEmail",
-    },
-    {
-      type: "input",
-      message: "What is your engineer's GitHub username?",
-      name: "engineerGithub",
-    },
-    
+const ManagerQuestions = [
+  {
+    type: "input",
+    message: "What is the team manager's name?",
+    name: "managerName",
+  },
+  {
+    type: "input",
+    message: "What is the team manager's id?",
+    name: "managerId",
+  },
+  {
+    type: "input",
+    message: "What is the team manager's email?",
+    name: "managerEmail",
+  },
+  {
+    type: "input",
+    message: "What is the team manager's office number?",
+    name: "managerNumber",
+  },
 ];
 
-function init() {
-    inquirer.prompt(questions).then((response) => {
+const internQuestions = [
+  // intern
+  {
+    type: "input",
+    message: "What is your intern's name?",
+    name: "internName",
+  },
+  {
+    type: "input",
+    message: "What is your intern's id?",
+    name: "internId",
+  },
+  {
+    type: "input",
+    message: "What is your intern's email?",
+    name: "internEmail",
+  },
+  {
+    type: "input",
+    message: "What is your intern's school?",
+    name: "internSchool",
+  },
+];
 
-        // const team = [];
-        // push manager to team arr
-        
+const engineerQuestions = [
+  // engineer
+  {
+    type: "input",
+    message: "What is your engineer's name?",
+    name: "engineerName",
+  },
+  {
+    type: "input",
+    message: "What is your engineer's id?",
+    name: "engineerId",
+  },
+  {
+    type: "input",
+    message: "What is your engineer's email?",
+    name: "engineerEmail",
+  },
+  {
+    type: "input",
+    message: "What is your engineer's GitHub username?",
+    name: "engineerGithub",
+  },
+];
 
-        // if (response.additionalTeamMember) return inquirer.prompt( intern or engineer question array)
-        // push team member to team arr 
+const moreTeamMembers = [
+  {
+    type: "list",
+    message:
+      "Which type of team member would you like to add?",
+    pageSize: 3,
+    choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+    name: "additionalTeamMember",
+  },
+];
 
-        // fs.writeFile('dist/index.html', generatedMarkup(team), (err) => {
-        //     if (err) throw console.error('Something went wrong while generating the README');
-        //     console.log('The file has been created!');
-        // });
-    });
+async function init() {
+  const team = [];
+  const {
+    managerName,
+    managerId,
+    managerEmail,
+    managerNumber,
+  } = await inquirer.prompt(ManagerQuestions);
+
+  const manager = new Manager(
+    managerName,
+    managerId,
+    managerEmail,
+    managerNumber
+  );
+
+  // push manager to team arr
+  team.push(manager);
+
+  
+  const addMoreMembers = async () => {
+    const { additionalTeamMember } = await inquirer.prompt(moreTeamMembers);
+
+    if (additionalTeamMember === 'Intern') {
+      const { internName, internId, internEmail, internSchool } = await inquirer.prompt(internQuestions);
+
+      const newIntern = new Intern(internName, internId, internEmail, internSchool);
+
+      team.push(newIntern);
+      await addMoreMembers();
+    } else if (additionalTeamMember === 'Engineer') {
+      const { engineerName, engineerId, engineerEmail, engineerGithub } = await inquirer.prompt(engineerQuestions);
+      
+      const newEngineer = new Engineer(engineerName, engineerId, engineerEmail, engineerGithub);
+
+      team.push(newEngineer);
+      await addMoreMembers();
+    } else return;
+
+  }
+  await addMoreMembers();
+
+
+  fs.writeFile('dist/index.html', generatedMarkup(team), (err) => {
+      if (err) throw console.error('Something went wrong while generating the README');
+      console.log('The file has been created!');
+  });
 }
 
 init();
